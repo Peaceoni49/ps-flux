368570b6a39aef782314f8aad9df3caa79ea6a28
peaceoni <peaceoni49@yahoo.com>
2020-04-15 08:01:26 +0100

    App-route

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import AboutPage from "./AboutPage";
 function App() {
     const route=window.location.pathname;
     if(route === "/about") return <AboutPage/>;
+    return <HomePage/>
 }
 
 export default App;
