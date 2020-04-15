9eceaf8d1ed816b9f3bec51aa90c8cdc9b8bc08f
peaceoni <peaceoni49@yahoo.com>
2020-04-15 21:45:20 +0100

    get page

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
+import Header from "./common/Header";
+
 
 function App() {
+    function getPage() {
     const route=window.location.pathname;
     if(route === "/about") return <AboutPage/>;
     return <HomePage/>
+    }
+    return getPage();
 }
 
 export default App;
