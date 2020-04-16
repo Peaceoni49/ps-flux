3209f89e45fc143b576ff5fc8bca709b7f875168
peaceoni <peaceoni49@yahoo.com>
2020-04-16 23:24:03 +0100

    courses-setstate

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -5,6 +5,12 @@ import { getCourses } from "../Api/courseApi";
 class CoursesPage extends React.Component {
     state = {
       courses:[]
+    };
+
+    componentDidMount() {
+        getCourses().then(function(courses){
+            this.setState({courses: courses});
+        }) 
     }
     render () {
         return <h2>Courses</h2>
