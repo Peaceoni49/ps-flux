45a118896c20f9db0e8f0fc9fb638d17082bc93b
peaceoni <peaceoni49@yahoo.com>
2020-04-16 23:11:39 +0100

    courses-constructor

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { getCourses } from "../Api/courseApi";
 
 
 class CoursesPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            courses:[]
+        }
+    }
     render () {
         return <h2>Courses</h2>
     }
 }
 
-export default CousrsesPage;
+export default CoursesPage;
