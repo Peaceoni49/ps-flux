fa40f32b7d807960c5af798c3cf30169d8cf48bf
peaceoni <peaceoni49@yahoo.com>
2020-04-17 22:50:31 +0100

    useEffect

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,26 +1,15 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { getCourses } from "../Api/courseApi";
 
 
-class CoursesPage extends React.Component {
-    state = {
-      courses:[]
-    };
-
-    componentDidMount() {
-        getCourses().then(courses=> this.setState({ courses: courses})); 
-    }
-
-    renderRow(course) {
+function CoursesPage() {
+    const [courses, setCourses ] = useState([]);
+useEffect( ()=> {
+    getCourses().then(_courses=> setCourses(_courses));
+})
         return(
-        <tr>
-                    <td>{course.title}</td>
-                    <td>{course.authorid}</td>
-                    <td>{course.category}</td>
-                </tr>
-        )}
-    render () {
-        return <><h2>Courses</h2>
+         <>
+         <h2>Courses</h2>
         <table className="table">
             <thead>
                 <tr>
@@ -30,12 +19,19 @@ class CoursesPage extends React.Component {
                 </tr>
             </thead>
             <tbody>
-                { this.state.courses.map(this.renderRow)}
+                {courses.map(course => {
+                return(
+                <tr key={course.id}>
+                    <td>{course.title}</td>
+                    <td>{course.authorid}</td>
+                    <td>{course.category}</td>
+                </tr>
+                );
+                })}
             </tbody>
-        </table></>
-        
-        
+        </table> 
+        </>
+     );   
     }
-}
 
 export default CoursesPage;
